7ccdbbee970bf1fe56e5fb6af90479b476de4874
dcerver <121509667+dcerver@users.noreply.github.com>
2023-10-25 17:27:31 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(wappsto.longitude())
+    basic.showNumber(wappsto.latitude())
+    basic.pause(100)
+})
 wappsto.configureName("GPS Microbit")
 wappsto.configureValue(1, "Longitude", WappstoValueTemplate.Longitude)
 wappsto.configureValue(2, "Latitude", WappstoValueTemplate.Latitude)
+wappsto.configureValue(3, "Temperature", WappstoValueTemplate.Temperature)
+wappsto.configureValue(4, "Lightlevel", WappstoValueTemplate.Number)
+while (!(wappsto.connected())) {
+    basic.showIcon(IconNames.No)
+}
+basic.clearScreen()
 basic.forever(function () {
     wappsto.sendNumberToWappsto(wappsto.longitude(), 1, WappstoTransmit.OnChange)
     wappsto.sendNumberToWappsto(wappsto.latitude(), 2, WappstoTransmit.OnChange)
-    basic.showNumber(wappsto.longitude())
-    basic.showNumber(wappsto.latitude())
-    basic.pause(5000)
+    wappsto.sendNumberToWappsto(input.temperature(), 3, WappstoTransmit.OnChange)
+    wappsto.sendNumberToWappsto(input.lightLevel(), 4, WappstoTransmit.OnChange)
 })
