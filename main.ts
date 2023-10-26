6e420058ec309b1d50d80a60671dc8da661fb3cd
dcerver <121509667+dcerver@users.noreply.github.com>
2023-10-26 15:42:50 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.B, function () {
     basic.showNumber(wappsto.longitude())
     basic.showNumber(wappsto.latitude())
     basic.pause(100)
